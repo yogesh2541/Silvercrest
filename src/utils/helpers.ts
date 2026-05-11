7eecd97272b9c46eb0aa6a9fbbd179fda7aff587
yogesh0291 <yogesh@ulabsglobal.com>
2026-05-11 23:40:40 +0530

    currency change

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,7 @@
 export const formatPrice = (price: number): string => {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
   }).format(price);
 };
 
